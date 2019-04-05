2b6c5c40e7dfadb0c5454eaaa07dcf83e7458a56
ghalib5000 <ghalibahmed5000@gmail.com>
2019-04-05 23:41:08 +0500

    modified some numbers

--- Add_or_Subtract/Add_or_Subtract.cs
+++ Add_or_Subtract/Add_or_Subtract.cs
@@ -8,12 +8,12 @@ namespace RandonNumberThingy
             int num=0;
             if(i%2==0)
             {//ADD
-              num=  r.create_Randomizer(0,5);
+              num=  r.create_Randomizer(1,6);
               System.Console.WriteLine("Added {0}",num);
             }
             else
             {//SUBTRACT
-                 num=  r.create_Randomizer(0,5);
+                 num=  r.create_Randomizer(1,6);
                  num=num-(num*2);
                  
               System.Console.WriteLine("Subtracted {0}",num);
