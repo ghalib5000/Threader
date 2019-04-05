045ffc034fb34c93fa479ec7f3feed58f38d1f9a
ghalib5000 <ghalibahmed5000@gmail.com>
2019-04-05 21:43:40 +0500

    done making some refinements

--- Add_or_Subtract/Add_or_Subtract.cs
+++ Add_or_Subtract/Add_or_Subtract.cs
@@ -9,11 +9,14 @@ namespace RandonNumberThingy
             if(i%2==0)
             {//ADD
               num=  r.create_Randomizer(0,5);
+              System.Console.WriteLine("Added {0}",num);
             }
             else
             {//SUBTRACT
                  num=  r.create_Randomizer(0,5);
                  num=num-(num*2);
+                 
+              System.Console.WriteLine("Subtracted {0}",num);
             }
             return num;
         }  
--- Program.cs
+++ Program.cs
@@ -5,28 +5,41 @@ namespace RandonNumberThingy
 {
     class Program
     {
+     static   Item_Manager item = new Item_Manager();
         static void Main(string[] args)
         { 
+           
+            Randomizer r = new Randomizer(); 
+            int loops= r.create_Randomizer(500000,1000000);
             Stopwatch stwtch = new Stopwatch();
             stwtch.Start(); 
-             int temp=0;
-             Randomizer r = new Randomizer(); 
-             Item_Manager item = new Item_Manager();
-             Add_or_Subtract aos = new Add_or_Subtract();
-             
-        int loops= r.create_Randomizer(500000,1000000);
+  
 
-        for(int i=0;i<50;i++)
-        {//NUMBER OF THREADS MADE
-        Thread.Sleep(500);
+            for(int i=0;i<500;i++)
+            {//NUMBER OF THREADS MADE
+                Thread t = new Thread(work);    
+                t.Start();
+                t.Join();
+                int y= t.ManagedThreadId;
+                Console.WriteLine("the current thread number is {0}",y);
+            }
+          
+             
+        stwtch.Stop();
+        TimeSpan time = stwtch.Elapsed;
+        Console.WriteLine("the final item count is: "+Program.item.Item_Count);
+        Console.WriteLine("it took {0} hours {1} minutes {2} seconds {3} millisecondsto complete the task",time.Hours,time.Minutes,time.Seconds,time.Milliseconds);
+        }
+        static void work()
+        {
+        Randomizer r = new Randomizer();
+        int temp=0;        
+        
+        Add_or_Subtract aos = new Add_or_Subtract();     
+        Thread.Sleep(50);
         temp = aos.Get_Number(r.create_Randomizer(0,9));
-        item.Item_Count = item.Item_Count+temp;
+      item.Item_Count = item.Item_Count+temp;
         item.Item_Checker();
         }
-        stwtch.Stop();
-        double time = stwtch.ElapsedMilliseconds;
-        Console.WriteLine("the final item count is: "+item.Item_Count);
-        Console.WriteLine("it took "+ time+" milliseconds to complete the task");
     }
-}
 }
